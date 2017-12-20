3922b5a57838e8d579c91b46fd066789e425fe18
Tommy Lees <thomas.lees112@gmail.com>
2017-12-20 14:30:19 +0000

    refactored the tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
+
+  def test_home_page_returns_correct_html(self):
+    response = self.client.get('/') #pass the url we want to test
+
+    self.assertTemplateUsed(response, 'home.html')
+
+"""
+# way of looking without Django test client
 
   def test_root_url_resolves_to_home_page(self):
     found = resolve('/')
@@ -14,6 +23,9 @@ class HomePageTest(TestCase):
     request = HttpRequest()
     response = home_page(request)
     html = response.content.decode('utf8')
+    expected_html = render_to_string('home.html')
     self.assertTrue(html.startswith('<html>'))
     self.assertIn('<title>To-Do lists</title>', html)
-    self.assertTrue(html.endswith('</html>'))
+    self.assertTrue(html.strip().endswith('</html>'))
+    self.assertEqual(html, expected_html)
+"""
